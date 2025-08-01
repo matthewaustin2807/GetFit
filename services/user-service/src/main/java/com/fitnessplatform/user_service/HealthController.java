19b8e9438f2ffcd0614d223c3608c9746b50c55e
matthewchandra28 <matthew.chandra@sayurbox.com>
2025-08-01 15:40:24 -0700

    First Commit

---
+++ services/user-service/src/main/java/com/fitnessplatform/user_service/HealthController.java
@@ -0,0 +1,13 @@
+package com.fitnessplatform.user_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/health")
+  public String health() {
+    return "User Service is healthy! 🚀";
+  }
+}
